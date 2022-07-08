9c127c4137728cd516d4ee2dfdc6a63b74f51179
AthenGonsalves <90402433+AthenGonsalves@users.noreply.github.com>
2022-07-08 15:05:22 +0530

    Add files via upload

---
+++ src/Payment.js
@@ -0,0 +1,68 @@
+import /*{ useEffect, useState }*/ React from 'react';
+import './Payment.css';
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import { Link/*, useHistory*/ } from "react-router-dom";
+//import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+//import CurrencyFormat from "react-currency-format";
+//import { getBasketTotal } from "./reducer";
+//import axios from './axios';
+
+function Payment() {
+    const [{ basket, user }, dispatch] = useStateValue();
+
+    return (
+        <div className='payment'>
+            <div className='payment_container'>
+                <h1>
+                    Checkout (
+                        <Link to="/checkout">{basket?.length} items</Link>
+                        )
+                </h1>
+
+                {/* Payment section - delivery address */}
+                <div className='payment_section'>
+                    <div className='payment_title'>
+                        <h3>Delivery Address</h3>
+                    </div>
+                    <div className='payment_address'>
+                        <p>{user?.email}</p>
+                        <p>123 React Lane</p>
+                        <p>Los Angeles, CA</p>
+                    </div>
+                </div>
+
+                 {/* Payment section - review items */}
+                 <div className='payment_section'>
+                    <div className='payment_title'>
+                        <h3>Review items and delivery</h3>
+                    </div>
+                    <div className='payment_items'>
+                        {basket.map(item => (
+                            <CheckoutProduct
+                                id={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        ))}
+                    </div>
+                 </div>
+                
+
+                  {/* Payment section - payment method */}
+                <div className='payment_section'></div>
+                    <div className='payment_title'>
+                        <h3>Payment Method</h3>
+                    </div>
+                    <div className='payment_details'>
+                      {/*   */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Payment
